3d24128d9f0b4b952bf3228f1c8a2cad367c6136
Patrick Wang <wangyunze16@gmail.com>
2024-05-29 12:51:40 +0100

    Modified the program to use async operations and pandas dataframe instead of request and csv lb.

--- mass_add_books.py
+++ mass_add_books.py
@@ -1,79 +1,145 @@
-import requests
-import csv
+import aiohttp
+import argparse
+import asyncio
 import re
 import sys
+import pandas as pd
 
-username = 'admin'
-password = 'admin123'
-shelf_id = '1'
-booklist = 'Meine Bücher.csv'
-server_address = 'http://127.0.0.1:8083'
 
-error = False
-if not shelf_id.isdigit():
-    print('Error: Shelf_id is not a number')
-    sys.exit(1)
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog='Calibre-Web Mass Book Adder',
+        description='A utility tool to bulk add books to shelves in Calibre-Web',
+    )
 
-r = requests.session()
-try:
-    login_page = r.get(server_address + '/login')
-    token = re.search('<input type="hidden" name="csrf_token" value="(.*)">', login_page.text)
-    resp = r.post(server_address + '/login?next=/', data={'username':username,
-                                                         'password':password,
-                                                         'submit': '',
-                                                         'remember_me': 'on',
-                                                         'next': '/',
-                                                         "csrf_token": token.group(1)})
-except Exception as e:
-    print("Error connecting to calibre-web : %s" % e)
-    r.close()
-    sys.exit(1)
+    parser.add_argument(
+        '--username',
+        type=str,
+        help='The username of the Calibre-Web server',
+        default='admin',
+    )
+    parser.add_argument(
+        '--password',
+        type=str,
+        help='The password of the Calibre-Web server',
+        default='admin123',
+    )
+    parser.add_argument(
+        '--address',
+        type=str,
+        help='The address of the Calibre-Web server',
+        default='http://127.0.0.1:8083',
+    )
+    parser.add_argument(
+        '--shelf_id',
+        type=str,
+        help='The ID of the shelf to add the books to',
+        default='1',
+    )
+    parser.add_argument(
+        '--booklist',
+        type=str,
+        help='The path to the CSV file containing the list of books to add',
+        default='Meine Bücher.csv',
+    )
+    parser.add_argument(
+        '--concurrency',
+        type=int,
+        help='The number of concurrent requests to make',
+        default=20,
+    )
 
-headers = {'Referer': server_address + '/'}
-if "login" in resp.text or resp.status_code != 200:
-    print('Error: Could not log in to calibre-web')
-    r.close()
-    sys.exit(1)
-try:
-    if sys.version_info.major >= 3:
-        csv_f = open(booklist, newline='')
-    else:
-        csv_f = open(booklist)
-except Exception as e:
-    print("Error on opening bookslist file: %s" % e)
-    r.close()
-    sys.exit(1)
-with csv_f as csvfile:
-    reader = csv.DictReader(csvfile)
-    for current_row in reader:
-        if "id" or "\ufeffid" in current_row:
-            id = current_row.get('id', current_row.get("\ufeffid"))
-            if id.isdigit():
-                shelf_page = r.get(server_address + '/shelf/create')
-                token = re.search('<input type="hidden" name="csrf_token" value="(.*)">', shelf_page.text)
-                payload = {"csrf_token": token.group(1)}
-                add = r.post(server_address + '/shelf/add/' + shelf_id + '/' + id,
-                             data=payload,
-                             cookies=r.cookies,
-                             headers=headers)
-                if add.status_code != 200:
-                    print('Error: Failed to add book with id %s to shelf %s' % (id, shelf_id))
-                else:
-                    message = re.findall(u"id=\"flash_.*class=.*>(.*)</div>", add.content.decode('utf-8'))
-                    if not message:
-                        print('Error: Book with id %s already in shelf, or shelf not existing' % (id))
-                    else:
-                        print('Request to add Book with id %s to shelf was successfully send. Calibre-Web Response: %s' % (id, message[0]))
-            else:
-                print('Error: id %s is not a number' % id)
-        else:
-            print("No ID-field found in current row of csv file, check for seperation character (has to be ',') and spelling of 'id' field")
-            r.close()
+    return parser.parse_args()
+
+
+async def login(session: aiohttp.ClientSession,
+                address: str,
+                username: str,
+                password: str,
+                ) -> str:
+    try:
+        login_page = await session.get(address + '/login')
+        page_content = await login_page.text()
+        token = re.search('<input type="hidden" name="csrf_token" value="(.*)">', page_content)
+        login_response = await session.post(
+            address + '/login?next=/',
+            data={
+                'username': username,
+                'password': password,
+                'submit': '',
+                'remember_me': 'on',
+                'next': '/',
+                "csrf_token": token.group(1),
+            },
+        )
+        if "login" in login_response.text or login_response.status != 200:
+            print('Error: Could not log in to calibre-web')
             sys.exit(1)
-r.close()
-print("script finished successfully")
+
+        return token.group(1)
+    except Exception as e:
+        print(f"Error connecting to calibre-web : {e}")
+        sys.exit(1)
 
 
+async def add_book_to_shelf(session: aiohttp.ClientSession,
+                            sem: asyncio.Semaphore,
+                            token: str,
+                            address: str,
+                            shelf_id: str,
+                            book_id: str,
+                            ):
+    async with sem:
+        try:
+            payload = {"csrf_token": token}
+            headers = {'Referer': address + '/'}
+            post_response = await session.post(address + '/shelf/add/' + shelf_id + '/' + book_id,
+                                               data=payload,
+                                               headers=headers)
+
+            if post_response.status == 200:
+                message = re.findall(u"id=\"flash_.*class=.*>(.*)</div>", await post_response.text())
+                if not message:
+                    print(f'Error: Book with id {book_id} already in shelf, or shelf not existing')
+                else:
+                    print(
+                        f'Request to add Book with id {book_id} to shelf was successfully send. Calibre-Web Response: {message[0]}')
+            else:
+                print(f'Error: Failed to add book with id {book_id} to shelf {shelf_id}')
+        except Exception as e:
+            print(f"Error adding book with id {book_id} to shelf: {e}")
 
 
+async def main():
+    args = parse_arguments()
+    sem = asyncio.Semaphore(args.concurrency)
 
+    async with aiohttp.ClientSession() as session:
+        # Login to Calibre-Web and get the CSRF token
+        token = await login(session, args.address, args.username, args.password)
+
+        # Read the book ids into a pandas dataframe
+        try:
+            df = pd.read_csv(args.booklist)
+        except Exception as e:
+            print(f"Error on opening books list file: {e}")
+            sys.exit(1)
+
+        # Add the books to the shelf
+        for index, row in df.iterrows():
+            book_id = row.get('id', row.get("\ufeffid"))
+            if book_id.isdigit():
+                await add_book_to_shelf(
+                    session=session,
+                    sem=sem,
+                    token=token,
+                    address=args.address,
+                    shelf_id=args.shelf_id,
+                    book_id=book_id,
+                )
+            else:
+                print(f'Error: id {book_id} is not a number')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
